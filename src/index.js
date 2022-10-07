e27488d861cc1bd0433ac5f4d44aae8e778c2817
Oleksandr Lytvyn <tvinlee7@gmail.com>
2022-10-07 13:06:06 +0300

    add throw error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './css/styles.css';
 import { fetchCountries } from './scripts/fetchCountries';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 const inputRef = document.querySelector('#search-box');
+const countryListRef = document.querySelector('.country-list');
 
 const debouncedInput = debounce(onInput, DEBOUNCE_DELAY);
 inputRef.addEventListener('input', debouncedInput);
@@ -13,47 +15,39 @@ function onInput(event) {
 
   if (country !== '') {
     fetchCountries(country).then(createMarkup);
+    //   .catch(error => {
+    //     Notify.error('Oops, there is no country with that name');
+    //   });
+  } else if (!country) {
+    countryListRef.innerHTML = '';
   }
 }
 
 function createMarkupList(event) {
   let markupString = '';
-  const countryListRef = document.querySelector('.country-list');
   if (event.length <= 10) {
     for (let i = 0; i < event.length; i++) {
-      const capital = event[i].capital[0];
-      const population = event[i].population;
       const country = event[i].name.official;
-      const languages = Object.values(event[i].languages)[0];
       const flag = event[i].flag;
       markupString += `<li>
         <h3>${flag}${country}</h3>
     </ul>`;
     }
-    // console.log(markupString);
     countryListRef.innerHTML = markupString;
   }
 }
 
 function createMarkupInfo(event) {
   let markupString = '';
-  const countryListRef = document.querySelector('.country-list');
-  //   console.log(countryListRef);
   if (event.length === 1) {
     const capital = event[0].capital[0];
     const population = event[0].population;
     const country = event[0].name.official;
     const languages = Object.values(event[0].languages)[0];
     const flag = event[0].flag;
-    console.log(capital);
-    console.log(population);
-    console.log(country);
-    console.log(languages);
-    console.log(flag);
     for (let i = 0; i < event.length; i++) {
       markupString += `<li>
         <h3>${flag}${country}</h3>
- 
         <p>Capital:${capital}</p>
         <p>Population:${population}</p>
         <p>Languages:${languages}</p>
@@ -68,4 +62,13 @@ function createMarkupInfo(event) {
 function createMarkup(event) {
   createMarkupList(event);
   createMarkupInfo(event);
+  notifications(event);
+}
+
+function notifications(event) {
+  if (event.length >= 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.', {
+      clickToClose: true,
+    });
+  }
 }
--- src/scripts/fetchCountries.js
+++ src/scripts/fetchCountries.js
@@ -1,8 +1,12 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 export function fetchCountries(name) {
   return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flag,languages
 `).then(response => {
-    if (!response) {
-      throw new Error(response.status);
+    if (!response.ok) {
+      Notify.failure('Oops, there is no country with that name', {
+        clickToClose: true,
+      });
     }
     return response.json();
   });
